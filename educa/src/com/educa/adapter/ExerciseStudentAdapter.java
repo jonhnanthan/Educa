ae356226345c82955f5839087703c2b02397c6a3
jonhnanthan <jonhnanthan@gmail.com>
2014-12-09 15:41:09 -0300

    organiza imports. Ainda com problemas em meus commits

--- educa/src/com/educa/adapter/ExerciseStudentAdapter.java
+++ educa/src/com/educa/adapter/ExerciseStudentAdapter.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
