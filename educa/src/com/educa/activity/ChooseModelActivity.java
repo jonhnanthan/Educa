094370c32ddf4c22c40a1ee57829060011e3d467
AnaLuciaS <analuciat.12@gmail.com>
2015-02-22 17:24:18 -0300

    Inicio da criacao de um novo modelo de exercicio (Imagens)

--- educa/src/com/educa/activity/ChooseModelActivity.java
+++ educa/src/com/educa/activity/ChooseModelActivity.java
@@ -1,4 +1,3 @@
-
 package com.educa.activity;
 
 import android.app.Activity;
@@ -12,68 +11,81 @@ import android.widget.ImageButton;
 import com.educa.R;
 
 public class ChooseModelActivity extends Activity {
-    private ImageButton bt_multiple_choice, bt_complete, bt_match;
+	private ImageButton bt_multiple_choice, bt_complete, bt_match, bt_image;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_exercise_model);
-        bt_multiple_choice = (ImageButton) findViewById(R.id.bt_multiplechoice);
-        bt_complete = (ImageButton) findViewById(R.id.bt_complete);
-        bt_match = (ImageButton) findViewById(R.id.bt_match);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_choose_exercise_model);
+		bt_multiple_choice = (ImageButton) findViewById(R.id.bt_multiplechoice);
+		bt_complete = (ImageButton) findViewById(R.id.bt_complete);
+		bt_match = (ImageButton) findViewById(R.id.bt_match);
+		bt_image = (ImageButton) findViewById(R.id.bt_image);
 
-        bt_complete.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentComplete = new Intent(getApplicationContext(),
-                        CompleteExerciseStep1Activity.class);
-                startActivity(intentComplete);
-            }
-        });
+		bt_complete.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intentComplete = new Intent(getApplicationContext(),
+						CompleteExerciseStep1Activity.class);
+				startActivity(intentComplete);
+			}
+		});
 
-        bt_multiple_choice.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        MultipleChoiceExerciseStep1Activity.class);
-                startActivity(intent);
-            }
-        });
+		bt_multiple_choice.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),
+						MultipleChoiceExerciseStep1Activity.class);
+				startActivity(intent);
+			}
+		});
 
-        bt_match.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),
-                        ChooseMatchExerciseActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+		bt_match.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),
+						ChooseMatchExerciseActivity.class);
+				startActivity(intent);
+			}
+		});
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+		bt_image.setOnClickListener(new OnClickListener() {
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),
+						ImageMatchExerciseStep1.class);
+				startActivity(intent);
+			}
+		});
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.about:
-                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.help:
-            	Intent help = new Intent(getApplicationContext(), HelpActivity.class);
-                startActivity(help);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		switch (item.getItemId()) {
+		case R.id.about:
+			Intent intent = new Intent(getApplicationContext(),
+					AboutActivity.class);
+			startActivity(intent);
+			return true;
+		case R.id.help:
+			Intent help = new Intent(getApplicationContext(),
+					HelpActivity.class);
+			startActivity(help);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
