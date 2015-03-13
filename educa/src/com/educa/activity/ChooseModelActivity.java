0996f05729ea53bc08f835963e0e1066f1179cc7
jonhnanthan <jonhnanthan@gmail.com>
2015-03-13 14:25:52 -0300

    ajuste no menu na tela de exercicios

--- educa/src/com/educa/activity/ChooseModelActivity.java
+++ educa/src/com/educa/activity/ChooseModelActivity.java
@@ -66,7 +66,7 @@ public class ChooseModelActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.exercise_help, menu);
 		return true;
 	}
 
